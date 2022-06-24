afbfd10d87778c7e6e2f052350e3ee643255c239
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-06-24 16:57:08 +0600

    add scroll to top helper

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './common.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import { ScrollToTop } from './Helpers/Index';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
+      <ScrollToTop />
       <App />
     </React.StrictMode>
   </BrowserRouter>
