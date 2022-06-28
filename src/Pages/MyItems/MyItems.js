10e30c8566b85f3dd1c955b667959dc1090b1c9c
Imran-2022 <mdimranulhaque.202@gmail.com>
2022-06-28 22:52:59 +0600

    functionally complete

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -7,11 +7,11 @@ const MyItems = () => {
     const [myItems, setMyItems] = useState([]);
     useEffect(() => {
         axios.get(`http://localhost:8080/api/products/?email=${email}`)
-            .then(data =>{
+            .then(data => {
                 setMyItems(data.data)
             })
     }, [])
-// handle Delete 
+    // handle Delete 
 
     // handle single delete -> 
 
@@ -28,43 +28,53 @@ const MyItems = () => {
     return (
         <div>
             <p className='text-center fs-1 py-5'>my items</p>
-            <div className='py-5'>
-                <table>
-                    <thead>
-                        <tr >
-                            <th className="text-center p-2">#no</th>
-                            <th className="text-center p-2">Image</th>
-                            <th className="text-center p-2">name</th>
-                            <th className="text-center p-2">Quantity</th>
-                            <th className="text-center p-2">Supplier</th>
-                            <th className="text-center p-2">Operations</th>
-                        </tr>
-                    </thead>
-                    {
-                        myItems.map((data, i) => {
-                            const { _id, title, img, quantity, supplierName } = data
-                            // console.log(img)
-                            return (
-                                <tbody key={_id}>
-                                    <tr>
-                                        <td className="text-center">{i + 1}</td>
-                                        <td className="text-center">
-                                            <img width="60px" height="40px" src={img} alt="no images" />
-                                        </td>
-                                        <td className="text-start ps-5">{title}</td>
-                                        <td className="text-center">{quantity}</td>
-                                        <td className="text-center">{supplierName}</td>
-                                        <td className="text-center">
-                                            <Link to={`/update/${_id}`}>Update</Link> {" "}
-                                            <span onClick={() => handleSigleDelete(_id)}>delete</span></td>
+            {
+                myItems.length ? <div className='py-5'>
+                    <Link to='/add-items' className='btn btn-primary'>add New</Link>
+                    <table>
+                        <thead>
+                            <tr >
+                                <th className="text-center p-2">#no</th>
+                                <th className="text-center p-2">Image</th>
+                                <th className="text-center p-2">name</th>
+                                <th className="text-center p-2">Quantity</th>
+                                <th className="text-center p-2">Supplier</th>
+                                <th className="text-center p-2">Operations</th>
+                            </tr>
+                        </thead>
+                        {
+                            myItems.map((data, i) => {
+                                const { _id, title, img, quantity, supplierName } = data
+                                // console.log(img)
+                                return (
+                                    <tbody key={_id}>
+                                        <tr>
+                                            <td className="text-center">{i + 1}</td>
+                                            <td className="text-center">
+                                                <img width="60px" height="40px" src={img} alt="no images" />
+                                            </td>
+                                            <td className="text-start ps-5">{title}</td>
+                                            <td className="text-center">{quantity}</td>
+                                            <td className="text-center">{supplierName}</td>
+                                            <td className="text-center">
+                                                <Link to={`/update/${_id}`}>Update</Link> {" "}
+                                                <span onClick={() => handleSigleDelete(_id)}>delete</span></td>
 
-                                    </tr>
-                                </tbody>
-                            )
-                        })
-                    }
-                </table>
-            </div>
+                                        </tr>
+                                    </tbody>
+                                )
+                            })
+                        }
+                    </table>
+                </div> :
+                    <div className='py-5 m-5'>
+                        <p>You Haven't Added Any Items Yet</p>
+                        <Link to='/add-items' className='btn btn-primary'>add New</Link>
+
+                    </div>
+            }
+
+
         </div>
     );
 };
